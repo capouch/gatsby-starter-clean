889e0333557cb3e9f7928e4e002ad0f0c5d6889e
Brian Capouch <brianc@palaver.net>
2019-01-07 16:40:35 -0500

    Freshen up

--- pages/index.js
+++ pages/index.js
@@ -19,12 +19,9 @@ export default class Index extends React.Component {
           Food Information 6 January 2019
         </h1>
         <p><b>Monday Lunch</b></p>
-        <p>There is sliced bread in the freezer.  I'll get out four slices.
-        There's a baggie in the fridge with ham and cheese slices.  Also some
-        rice pudding for dessert.</p>
+        <p>He said he would have the Hoosier Stew for lunch.</p>
         <p><b>Monday Dinner</b></p>
-        <p>Hoosier Stew, in a pint container in the fridge.  Just needs put into a
-        bowl and warmed up.</p>
+        <p>He is planning to eat the shrimp alfredo Pat got set up for him.</p>
       </div>
     )
   }
